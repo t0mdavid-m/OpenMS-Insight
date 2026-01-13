8a3f02dcbb63672799f49b66c53217240a1a9b9f
Tom David Mueller <tom.mueller@beachouse.de>
2026-01-13 12:11:39 +0100

    chore: bump version to 0.1.5

--- openms_insight/__init__.py
+++ openms_insight/__init__.py
@@ -16,7 +16,7 @@ from .core.registry import get_component_class, register_component
 from .core.state import StateManager
 from .rendering.bridge import clear_component_annotations, get_component_annotations
 
-__version__ = "0.1.0"
+__version__ = "0.1.5"
 
 __all__ = [
     # Core
